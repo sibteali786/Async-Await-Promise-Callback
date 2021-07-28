e8142da3493982356beaab4e03cdf15cb43d3026
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-07-28 16:35:05 +0500

    Added more callback functions to explain callback hell

--- synchronus.js
+++ synchronus.js
@@ -1,11 +1,29 @@
 console.log("Before Preparing food");
 
-function prepareFood() {
+function prepareFood(callback) {
   setTimeout(() => {
     console.log("Prepare Food");
-  });
+    callback("Food is Ready");
+  }, 10);
 }
 
-prepareFood();
+function mycallback(value) {
+  console.log("Value =", value);
+  prepareFrenchToast(frenchcallback);
+}
+
+function frenchcallback(value) {
+  console.log("French Toast Callback =", value);
+}
+function prepareFrenchToast(callback) {
+  setTimeout(() => {
+    console.log("Prepare French toast");
+    callback("french toast is ready");
+  }, 20);
+}
+prepareFood(mycallback);
+// prepareFood((value)=>{
+//     console.log("Value =",value);
+// })
 
 console.log("After preparing food dinner ready");
